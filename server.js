e842e23f02a2916ba49e3637aacd0735c32cb3dd
Abhishek Pillai <36700271+noobie-developer@users.noreply.github.com>
2018-02-22 00:16:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.sendFile(createTemplate(articelOne));
+  res.sendFile(createTemplate(articleOne));
 });
 
 app.get('/article-three', function (req, res) {
